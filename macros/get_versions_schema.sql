401700e8c6bcece43c225529a155a9b292131d84
dalongrong <1141591465@qq.com>
2018-10-19 11:05:31 +0800

    add some demo

---
+++ macros/get_versions_schema.sql
@@ -0,0 +1,4 @@
+{% macro get_versions_schema() -%}
+{%- set defaultname =demo -%}
+
+{%- endmacro %}
--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -6,5 +6,20 @@
 -- Try changing 'view' to 'table', then re-running dbt
 {{ config(materialized='view') }}
 
-
-select 1 as id
+select 1 as id, 'v1' as version,'default' as v,'default' as v2
+union all
+select 2,'v2','default','default'
+union all
+select 3,'v3','default','default'
+union all
+select 4,'v4','default','default'
+union all
+select 5,'v5','default','default'
+union all
+select 6,'v6','default','default'
+union all
+select 7,'v7','default','default'
+union all
+select 8,'v8','default','default'
+union all
+select 9,'v9','default','default'
